3c8daa2767b01faf5c495539edfadab2c2de9332
Confidence Madiope <confidencem97@gmail.com>
2022-08-01 13:58:36 +0200

    ADD: add document button

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Docs() {
   return (
-    <div>
-      Docs
+    <div className='docs-main'>
+      <h1>Docs Clone</h1>
+
+      <button className='add-docs'>
+        Add a Document
+      </button>
     </div>
   )
 }
